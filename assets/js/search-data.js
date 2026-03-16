7d20b78ebf8d115e1be95c6abd6dd15037d27e3d
skrich <54861789+skrich@users.noreply.github.com>
2026-03-16 19:18:05 +0000

    Deploying to gh-pages from @ skrich/skrich.github.io@0207baa152d743214c15985d4ba3a1fd24e4cef8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,8 +42,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
+            },},{id: "projects-sentence-comprehension-in-person-with-aphasia",
+          title: 'sentence comprehension in person with aphasia',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
