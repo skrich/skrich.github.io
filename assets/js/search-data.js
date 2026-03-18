ee580f5164754c039e3f8bdf40a4d1c087e217e8
skrich <54861789+skrich@users.noreply.github.com>
2026-03-18 03:11:33 +0000

    Deploying to gh-pages from @ skrich/skrich.github.io@ff96443360f102403e9df61b1544d8d8f7064337 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv-resume/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
