01d8c7d32368b98c70c36a0ffe54f208ba73dd19
skrich <54861789+skrich@users.noreply.github.com>
2026-03-18 04:32:17 +0000

    Deploying to gh-pages from @ skrich/skrich.github.io@22cf9eb7b1b5da095a47d46c3f993c818d0ce196 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A selection of finished and ongoing projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "",
