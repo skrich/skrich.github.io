60cfae976f328dee06345a1f755b8da880e0f99f
skrich <54861789+skrich@users.noreply.github.com>
2026-07-13 01:37:48 +0000

    Deploying to gh-pages from @ skrich/skrich.github.io@731c511e5bc0d506bf3faecc55c0d2b2203ac424 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -60,8 +60,8 @@ ninja.data = [{
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
+            },},{id: "projects-learning-emotional-categories",
+          title: 'learning emotional categories',
           description: "a project with no image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
