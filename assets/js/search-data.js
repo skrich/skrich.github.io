2db285710d1e880045f8fbce29e18cf8c8d78a94
skrich <54861789+skrich@users.noreply.github.com>
2026-07-13 01:40:39 +0000

    Deploying to gh-pages from @ skrich/skrich.github.io@cec48a16543268fba0edb130c86108b9a595bd0a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -75,8 +75,8 @@ ninja.data = [{
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
+            },},{id: "projects-equity-in-linguistics",
+          title: 'Equity in Linguistics',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
